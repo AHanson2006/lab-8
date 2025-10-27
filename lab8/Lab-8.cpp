e3580b6d3347cdc932238df091aec9fdd4d8b83a
AHanson2006 <a-hanson.3@onu.edu>
2025-10-27 18:48:55 -0400

    Vowel Counter

--- lab8/Lab-8.cpp
+++ lab8/Lab-8.cpp
@@ -2,29 +2,42 @@
 #include <string>
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main() {
 	string str;
 	int charCount = 0;
 
+
 	cout << "Enter string: ";
 	getline(cin, str);
 
-	cout << endl << countCharacter(str) << " Characters.";
+	cout << endl << countVowel(str) << " Vowels.";
 
 	
 }
 
-int countCharacter(string str) {
+int countVowel(string str) {
 	
-	int charCount = 0;
+	int vowelCount = 0;
 	
 	for (int i = 0; i < size(str); i++) {
-		if (str[i] != ' ') {
-			charCount++;
+		if (str[i] == 'a' || str[i] == 'A') {
+			vowelCount++;
+		}
+		else if (str[i] == 'e' || str[i] == 'E') {
+			vowelCount++;
+		}
+		else if (str[i] == 'i' || str[i] == 'I') {
+			vowelCount++;
+		}
+		else if (str[i] == 'o' || str[i] == 'O') {
+			vowelCount++;
+		}
+		else if (str[i] == 'u' || str[i] == 'U') {
+			vowelCount++;
 		}
 	}
 
-	return charCount;
+	return vowelCount;
 }
